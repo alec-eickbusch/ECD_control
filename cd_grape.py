c322e10c9377a548e08568162071f0e9c54b1cca
awe4 <alec.eickbusch@yale.edu>
2020-08-31 15:16:11 -0400

    Initial analytic optimization

--- cd_grape.py
+++ cd_grape.py
@@ -8,6 +8,7 @@ import numpy as np
 import qutip as qt 
 from helper_functions import plot_wigner
 from scipy.optimize import minimize
+import scipy.optimize
 
 class CD_grape:
 
@@ -20,14 +21,14 @@ class CD_grape:
         self.initial_state = initial_state
         self.target_state = target_state
         self.N_blocks = N_blocks
-        self.alphas = np.array(init_alphas, dtype=np.complex64) if init_alphas is not None \
-            else np.zeros(N_blocks, dtype=np.complex64)
-        self.betas = np.array(init_betas, dtype=np.complex64) if init_betas is not None \
-            else np.zeros(N_blocks, dtype=np.complex64)
-        self.phis = np.array(init_phis, dtype=np.float32) if init_phis is not None \
-            else np.zeros(N_blocks, dtype=np.float32)
-        self.thetas = np.array(init_thetas, dtype=np.float32) if init_thetas is not None \
-            else np.zeros(N_blocks, dtype=np.float32)
+        self.alphas = np.array(init_alphas, dtype=np.complex128) if init_alphas is not None \
+            else np.zeros(N_blocks, dtype=np.complex128)
+        self.betas = np.array(init_betas, dtype=np.complex128) if init_betas is not None \
+            else np.zeros(N_blocks, dtype=np.complex128)
+        self.phis = np.array(init_phis, dtype=np.float64) if init_phis is not None \
+            else np.zeros(N_blocks, dtype=np.float64)
+        self.thetas = np.array(init_thetas, dtype=np.float64) if init_thetas is not None \
+            else np.zeros(N_blocks, dtype=np.float64)
         self.N = self.initial_state.dims[0][0]
         self.N2 = self.initial_state.dims[0][1]
         self.max_abs_alpha = max_abs_alpha
@@ -47,10 +48,10 @@ class CD_grape:
         rho_beta = np.random.uniform(-self.max_abs_beta, self.max_abs_beta, self.N_blocks)
         phis = np.random.uniform(-np.pi, np.pi, self.N_blocks)
         thetas = np.random.uniform(0,np.pi,self.N_blocks)
-        self.alphas = np.array(np.exp(1j*ang_alpha)*rho_alpha, dtype=np.complex64)
-        self.betas = np.array(np.exp(1j*ang_beta)*rho_beta, dtype=np.complex64)
-        self.phis = np.array(phis, dtype=np.complex64)
-        self.thetas = np.array(thetas, dtype=np.complex64)
+        self.alphas = np.array(np.exp(1j*ang_alpha)*rho_alpha, dtype=np.complex128)
+        self.betas = np.array(np.exp(1j*ang_beta)*rho_beta, dtype=np.complex128)
+        self.phis = np.array(phis, dtype=np.complex128)
+        self.thetas = np.array(thetas, dtype=np.complex128)
 
     def D(self, alpha):
         return (alpha*self.a.dag() - np.conj(alpha)*self.a).expm()
@@ -114,36 +115,35 @@ class CD_grape:
     def fid_and_grad_fid(self, alphas, betas, phis, thetas):
         psi_fwd = self.forward_states(alphas, betas, phis, thetas)
         psi_bwd = self.reverse_states(alphas, betas, phis, thetas)
-        overlap = (psi_bwd[0]*psi_fwd[-1]).full()[0][0] #might be positive or negative
+        overlap = (psi_bwd[0]*psi_fwd[-1]).full()[0][0] #might be complex
         fid = np.abs(overlap)**2
         
-        dalphar = np.zeros(N_blocks, dtype=np.complex64)
-        dalphai = np.zeros(N_blocks, dtype=np.complex64)
-        dbetar = np.zeros(N_blocks, dtype=np.complex64)
-        dbetai = np.zeros(N_blocks, dtype=np.complex64)
-        dphi = np.zeros(N_blocks, dtype=np.complex64)
-        dtheta = np.zeros(N_blocks, dtype=np.complex64)
+        dalphar = np.zeros(N_blocks, dtype=np.complex128)
+        dalphai = np.zeros(N_blocks, dtype=np.complex128)
+        dbetar = np.zeros(N_blocks, dtype=np.complex128)
+        dbetai = np.zeros(N_blocks, dtype=np.complex128)
+        dphi = np.zeros(N_blocks, dtype=np.complex128)
+        dtheta = np.zeros(N_blocks, dtype=np.complex128)
 
         for i in range(self.N_blocks): #question: do I take the real part?
             for j in [1,2,3]:
                 k = 3*i + j
-                print(k)
-                print(-(k+1))
                 if j == 1:
-                    dalphar[i] = (psi_bwd[-(k+1)]*self.dalphar_dD_mul(alphas[i])*psi_fwd[k]).full()[0][0]
-                    dalphai[i] = (psi_bwd[-(k+1)]*self.dalphai_dD_mul(alphas[i])*psi_fwd[k]).full()[0][0]
-                if j == 2:
-                    dbetar[i] = (psi_bwd[-(k+1)]*self.dbetar_dCD_mul(betas[i])*psi_fwd[k]).full()[0][0]
-                    dbetai[i] = (psi_bwd[-(k+1)]*self.dbetai_dCD_mul(betas[i])*psi_fwd[k]).full()[0][0]
-                if j == 3:
                     dphi[i] = (psi_bwd[-(k+1)]*self.dphi_dR_mul(phis[i], thetas[i])*psi_fwd[k]).full()[0][0]
                     dtheta[i] = (psi_bwd[-(k+1)]*self.dtheta_dR_mul(phis[i], thetas[i])*psi_fwd[k]).full()[0][0]
-        dalphar = np.real(overlap*np.conj(dalphar))/np.abs(overlap)
-        dalphai = np.real(overlap*np.conj(dalphar))/np.abs(overlap)
-        dbetar = np.real(overlap*np.conj(dbetar))/np.abs(overlap)
-        dbetai = np.real(overlap*np.conj(dbetai))/np.abs(overlap)
-        dphi = np.real(overlap*np.conj(dphi))/np.abs(overlap)
-        dtheta = np.real(overlap*np.conj(dtheta))/np.abs(overlap)
+                if j==2:
+                    dalphar[i] = (psi_bwd[-(k+1)]*self.dalphar_dD_mul(alphas[i])*psi_fwd[k]).full()[0][0]
+                    dalphai[i] = (psi_bwd[-(k+1)]*self.dalphai_dD_mul(alphas[i])*psi_fwd[k]).full()[0][0]
+                if j == 3:
+                    dbetar[i] = (psi_bwd[-(k+1)]*self.dbetar_dCD_mul(betas[i])*psi_fwd[k]).full()[0][0]
+                    dbetai[i] = (psi_bwd[-(k+1)]*self.dbetai_dCD_mul(betas[i])*psi_fwd[k]).full()[0][0]
+                    
+        dalphar = 2*np.abs(overlap)*np.real(overlap*np.conj(dalphar))/np.abs(overlap)
+        dalphai = 2*np.abs(overlap)*np.real(overlap*np.conj(dalphai))/np.abs(overlap)
+        dbetar = 2*np.abs(overlap)*np.real(overlap*np.conj(dbetar))/np.abs(overlap)
+        dbetai = 2*np.abs(overlap)*np.real(overlap*np.conj(dbetai))/np.abs(overlap)
+        dphi = 2*np.abs(overlap)*np.real(overlap*np.conj(dphi))/np.abs(overlap)
+        dtheta = 2*np.abs(overlap)*np.real(overlap*np.conj(dtheta))/np.abs(overlap)
  
         return fid, dalphar, dalphai, dbetar, dbetai, dphi, dtheta
 
@@ -152,20 +152,19 @@ class CD_grape:
         betas = self.betas if betas is None else betas
         phis = self.phis if phis is None else phis
         thetas = self.thetas if thetas is None else thetas
-        U = self.U_tot(alphas, betas, phis, thetas)
-        return U*self.initial_state
-
+        psi = self.initial_state
+        for i in range(self.N_blocks):
+            psi = self.R(phis[i],thetas[i])*psi
+            psi = self.D(alphas[i])*psi
+            psi = self.CD(betas[i])*psi
+        return psi
+    
     def fidelity(self, alphas=None, betas=None, phis = None, thetas=None):
         alphas = self.alphas if alphas is None else alphas
         betas = self.betas if betas is None else betas
         phis = self.phis if phis is None else phis
         thetas = self.thetas if thetas is None else thetas
-        #r= self.target_state.dag() *\
-            #self.final_state(alphas, betas, phis, thetas)
-        #return np.real(r.full()[0][0])
-        r = self.target_state.dag()*self.forward_states(alphas,betas,phis,thetas)[-1]
-        r2 = self.reverse_states(alphas,betas,phis,thetas)[-1]*self.initial_state
-        return np.real(r2.full()[0][0])    
+        return np.abs((self.target_state.dag() * self.final_state(alphas,betas,phis,thetas)).full()[0][0])**2
     
     def plot_initial_state(self):
         plot_wigner(self.initial_state)
@@ -194,25 +193,53 @@ class CD_grape:
         self.phis = phis
         self.thetas = thetas
         f = self.fidelity(alphas,betas,phis,thetas)
-        if self.last_fidelity != f:
-            print('fid: %.3f' % f, end='\r')
-        self.last_fidelity = f
+        print('fid: %.3f' % f, end='\r')
         return -f
+
+    def cost_function_analytic(self, parameters):
+        alphas_r = parameters[0:self.N_blocks]
+        alphas_i = parameters[self.N_blocks:2*self.N_blocks]
+        betas_r = parameters[2*self.N_blocks:3*self.N_blocks]
+        betas_i = parameters[3*self.N_blocks:4*self.N_blocks]
+        phis = parameters[4*self.N_blocks:5*self.N_blocks]
+        thetas = parameters[5*self.N_blocks:6*self.N_blocks]
+        alphas = alphas_r + 1j*alphas_i
+        betas = betas_r + 1j*betas_i
+        self.alphas = alphas
+        self.betas = betas
+        self.phis = phis
+        self.thetas = thetas
+        f, dalphar, dalphai, dbetar, dbetai, dphi, dtheta = self.fid_and_grad_fid(alphas,betas,phis,thetas)
+        gradf = np.concatenate([dalphar,dalphai, dbetar, dbetai, dphi, dtheta])
+        print('fid: %.3f' % f, end='\r')
+        return (-f, -gradf)
     
-    def optimize(self, maxiter = None):
-        self.last_fidelity = -2
+    def optimize(self, maxiter = 1e4):
         init_params = \
-        np.array([np.real(self.alphas),np.imag(self.alphas),
+        np.array(np.concatenate([np.real(self.alphas),np.imag(self.alphas),
                   np.real(self.betas), np.imag(self.betas),
-                  self.phis, self.thetas],dtype=np.float32)
+                  self.phis, self.thetas]),dtype=np.float64)
         bounds = np.concatenate(
             [[(-self.max_abs_alpha,self.max_abs_alpha) for _ in range(2*N_blocks)],\
             [(-self.max_abs_beta,self.max_abs_beta) for _ in range(2*N_blocks)],\
             [(-np.pi,np.pi) for _ in range(N_blocks)],\
             [(0,np.pi) for _ in range(N_blocks)]])
         result = minimize(self.cost_function,x0=init_params,method='L-BFGS-B',
-                          bounds = bounds, jac=False, options={'disp':True,
-                                                               'maxiter':maxiter})
+                          bounds = bounds, jac=False, options={'maxiter':maxiter})
+        return result
+
+    def optimize_analytic(self, check=False, maxiter = 1e4, gtol=1e-10, ftol=1e-10):
+        init_params = \
+        np.array(np.concatenate([np.real(self.alphas),np.imag(self.alphas),
+                  np.real(self.betas), np.imag(self.betas),
+                  self.phis, self.thetas]),dtype=np.float64)
+        bounds = np.concatenate(
+            [[(-self.max_abs_alpha,self.max_abs_alpha) for _ in range(2*N_blocks)],\
+            [(-self.max_abs_beta,self.max_abs_beta) for _ in range(2*N_blocks)],\
+            [(-np.pi,np.pi) for _ in range(N_blocks)],\
+            [(0,np.pi) for _ in range(N_blocks)]])
+        result = minimize(self.cost_function_analytic,x0=[init_params],method='L-BFGS-B',
+                          bounds = bounds, jac=True), options={'maxiter':maxiter,'gtol':gtol,'ftol':ftol})
         return result
     
 #%%
@@ -221,21 +248,37 @@ if __name__ == '__main__':
     N2 = 2
     N_blocks = 4
     init_state = qt.tensor(qt.basis(N,0),qt.basis(N2,0))
-    #target_state = qt.tensor(qt.basis(N,1),qt.basis(N2,0))
-    target_state = qt.tensor((qt.coherent(N,np.sqrt(2)) + qt.coherent(N,-np.sqrt(2))).unit(),qt.basis(N2,0))
-    init_alphas = np.random.uniform(low=-3,high=+3,size=N_blocks)
+    target_state = qt.tensor(qt.basis(N,1),qt.basis(N2,0))
+    #target_state = qt.tensor((qt.coherent(N,np.sqrt(2)) + qt.coherent(N,-np.sqrt(2))).unit(),qt.basis(N2,0))
     a = CD_grape(init_state, target_state, N_blocks, max_abs_alpha=1,
                  max_abs_beta = 1)
     a.randomize()
-    if 0:
-        a.plot_initial_state()
-        a.plot_final_state()
-        a.plot_target_state()
+    #if 1:
+        #a.plot_initial_state()
+        #a.plot_final_state()
+        #a.plot_target_state()
     print(a.fidelity())
+#%% 
+init_params = \
+        np.array(np.concatenate([np.real(a.alphas),np.imag(a.alphas),
+                  np.real(a.betas), np.imag(a.betas),
+                  a.phis, a.thetas]),dtype=np.float64)
 
+f, df = a.cost_function_analytic(init_params)
 
+test_d = np.zeros_like(init_params)
+test_d[4] = 0.001
+f2, df2 = a.cost_function_analytic(init_params + test_d)
+
+test_grad = (f2-f)/0.001
+print(df)
+print(test_grad)
+print(f)
+print(f2)
+#%%
+a.optimize()
 # %%
-a.optimize(1000)
+a.optimize_analytic()
 # %%
 a.plot_final_state()
 print('alphas:' + str(a.alphas))
