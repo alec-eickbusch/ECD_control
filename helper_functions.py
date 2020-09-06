aaa82e6081f2da1184e87d39d2e3abd3b87bcd30
Alec Eickbusch <alec.eickbusch@yale.edu>
2020-09-05 20:52:35 -0400

    Getting code to work on fpga (py2)

--- helper_functions.py
+++ helper_functions.py
@@ -1,11 +1,11 @@
 #%%
 import numpy as np
-import qutip as qt
 import matplotlib.pyplot as plt
 plt.rcParams.update({'font.size': 16,'pdf.fonttype':42,'ps.fonttype':42})
 from scipy.interpolate import interp1d
 from scipy.integrate import solve_ivp, quad
 #%%
+'''
 def plot_wigner(state, xvec = np.linspace(-5,5,81), tensor_state=True):
     if tensor_state:
         state = qt.ptrace(state, 0)
@@ -16,7 +16,7 @@ def plot_wigner(state, xvec = np.linspace(-5,5,81), tensor_state=True):
     plt.axvline(0,linestyle='--', color='black',alpha=0.4)
     plt.colorbar()
     plt.grid()
-
+'''
 def plot_pulse(epsilon, Omega):
     ts = np.arange(len(epsilon))
     plt.plot(ts,1e3*np.real(epsilon)/2/np.pi,label='Re(epsilon)')
