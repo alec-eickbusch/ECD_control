Fixing direction of rotation pulse

basic_pulses.py
@@ -56,7 +56,7 @@ def rotate(theta, phi=0, sigma=8, chop=6, dt=1):
     energy = np.trapz(wave, dx = dt)
     amp = 1/energy
     wave = (1 + 0j)*wave
-    return (-theta/(2.0)) * amp * np.exp(1j*phi) * wave
+    return (theta/(2.0)) * amp * np.exp(1j*phi) * wave
 
 #displace cavity by an amount alpha
 def disp_gaussian(alpha, sigma=8, chop=6, dt=1):