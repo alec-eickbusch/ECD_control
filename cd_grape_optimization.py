Correct D, CD, and theta derivatives

cd_grape_optimization.py
@@ -11,6 +11,7 @@ from scipy.optimize import minimize
 import scipy.optimize
 from datetime import datetime
 
+#TODO: Handle cases when phi, theta outside range.
 
 class OptFinishedException(Exception):
     def __init__(self, msg, CD_grape_obj):
@@ -79,6 +80,9 @@ class CD_grape:
         #return self.R(0,np.pi)*((beta*self.a.dag() - np.conj(beta)*self.a)*(self.sz/2.0)).expm()
         #temp removing pi pulse from CD for analytic opt testing
         return ((beta*self.a.dag() - np.conj(beta)*self.a)*(self.sz/2.0)).expm()
+        #zz = qt.tensor(qt.identity(self.N),qt.ket2dm(qt.basis(self.N2,0)))
+        #oo = qt.tensor(qt.identity(self.N), qt.ket2dm(qt.basis(self.N2, 1)))
+        #return self.D(beta/2.0)*zz + self.D(-beta/2.0)*oo
 
     def R(self, phi, theta):
         return (-1j*(theta/2.0)*(np.cos(phi)*self.sx + np.sin(phi)*self.sy)).expm()
@@ -92,17 +96,16 @@ class CD_grape:
         return 1j*(self.a.dag() + self.a - (np.conj(alpha) + alpha)/2.0)
 
     def dbetar_dCD_mul(self, beta):
-        return (self.a.dag() - self.a - (self.sz/2.0)*(np.conj(beta) - beta)/2.0)
+        return 0.5*(self.sz*(self.a.dag() - self.a) - ((np.conj(beta) - beta)/4.0))
 
     def dbetai_dCD_mul(self, beta):
-        return 1j*(self.a.dag() + self.a - (self.sz/2.0)*(np.conj(beta) + beta)/2.0)
+        return 1j*0.5*(self.sz*(self.a.dag() + self.a) - ((np.conj(beta) + beta)/4.0))
 
     def dtheta_dR_mul(self, phi, theta):
-        return (1j/2.0)*((np.sinc(theta/np.pi))*(self.sx*np.cos(phi)+self.sy*np.sin(phi))+\
-            (1 - np.sinc(theta/np.pi))*(self.sx*np.cos(phi)**2 + self.sy*np.sin(phi)**2))
+        return (-1j/2.0)*(self.sx*np.cos(phi) + self.sy*np.sin(phi))
     
     def dphi_dR_mul(self, phi, theta):
-        return (1j/2.0)*((np.sin(theta))*(self.sy*np.cos(phi)-self.sx*np.sin(phi)) +\
+        return (-1j/2.0)*((np.sin(theta))*(self.sy*np.cos(phi)-self.sx*np.sin(phi)) +\
             (1-np.cos(phi))*self.sz + np.cos(phi)*np.sin(phi)*(theta - np.sin(theta))*(self.sy - self.sx))
 
     def U_block(self, alpha, beta, phi, theta):
@@ -239,10 +242,10 @@ class CD_grape:
 
     #TODO: include final rotation and displacement
     def cost_function_analytic(self, parameters):
-        alphas_r = parameters[0:self.N_blocks]
-        alphas_i = parameters[self.N_blocks:2*self.N_blocks]
-        betas_r = parameters[2*self.N_blocks:3*self.N_blocks]
-        betas_i = parameters[3*self.N_blocks:4*self.N_blocks]
+        betas_r = parameters[0:self.N_blocks]
+        betas_i = parameters[self.N_blocks:2*self.N_blocks]
+        alphas_r = parameters[2*self.N_blocks:3*self.N_blocks]
+        alphas_i = parameters[3*self.N_blocks:4*self.N_blocks]
         phis = parameters[4*self.N_blocks:5*self.N_blocks]
         thetas = parameters[5*self.N_blocks:6*self.N_blocks]
         alphas = alphas_r + 1j*alphas_i
@@ -291,12 +294,12 @@ class CD_grape:
     #TODO: understand gtol and ftol
     def optimize_analytic(self, check=False, maxiter = 1e4, gtol=1e-4, ftol=1e-4):
         init_params = \
-        np.array(np.concatenate([np.real(self.alphas),np.imag(self.alphas),
-                  np.real(self.betas), np.imag(self.betas),
+        np.array(np.concatenate([np.real(self.betas),np.imag(self.betas),
+                  np.real(self.alphas), np.imag(self.alphas),
                   self.phis, self.thetas]),dtype=np.float64)
         bounds = np.concatenate(
-            [[(-self.max_alpha,self.max_alpha) for _ in range(2*N_blocks)],\
-            [(-self.max_beta,self.max_beta) for _ in range(2*N_blocks)],\
+            [[(-self.max_beta,self.max_beta) for _ in range(2*N_blocks)],\
+            [(-self.max_alpha,self.max_alpha) for _ in range(2*N_blocks)],\
             [(-1000,1000) for _ in range(N_blocks)],\
             [(-1000,1000) for _ in range(N_blocks)]])
             #note that for the cyclic variables the bounds are hard to define, you don't want to

testing/analytic_opt_testing.py
@@ -1,48 +1,95 @@
 #%%
+%load_ext autoreload
+%autoreload 2
+import importlib
 from CD_GRAPE.cd_grape_optimization import *
 from CD_GRAPE.basic_pulses import *
 from CD_GRAPE.helper_functions import *
-from CD_GRAPE.analysis import *
 import numpy as np
 import qutip as qt
 import matplotlib.pyplot as plt
 #%%
-N = 30
-N2 = 3
-epsilon_m = 2*np.pi*1e-3*300.0  # maximum displacement rate
-alpha0 = 50  # maximum displacement before things break down
-Ec_GHz = 0.19267571  # measured anharmonicity
-Ec = (2*np.pi) * Ec_GHz
-chi_MHz = 0.03
-chi = 2*np.pi*1e-3*chi_MHz
-sigma = 6  # sigma for gaussian pulses
-chop = 4  # chop for gaussian pulses
-buffer_time = 4  # time between discrete pulses
-ring_up_time = 16  # Time to ring up for large CD pulses
-N_blocks = 0
+N = 150  # cavity hilbert space
+N2 = 2  # qubit hilbert space
+N_blocks = 1
+alpha = 1
 initial_state = qt.tensor(qt.basis(N, 0), qt.basis(N2, 0))
-cd_grape_obj = CD_grape(initial_state, N_blocks=N_blocks)
-sys = System(chi=chi, Ec=Ec, alpha0=alpha0,
-             sigma=sigma, chop=chop, epsilon_m=epsilon_m, buffer_time=buffer_time,
-             ring_up_time=ring_up_time)
-analysis_obj = CD_grape_analysis(cd_grape_obj, sys)
+target_state = qt.tensor(qt.coherent(N,alpha), qt.basis(N2,0))
+term_fid = 0.99
+#max alpha and beta are the maximum values of alpha and beta for optimization
+max_alpha = 5
+max_beta = 5
+name = "displace"
+saving_directory = "C:\\Users\\Alec Eickbusch\\Documents\\CD_grape_parameters\\"
+cd_grape_obj = CD_grape(initial_state, target_state, N_blocks,
+                        name=name, term_fid=term_fid,
+                        max_alpha=max_alpha, max_beta=max_beta,
+                        saving_directory=saving_directory)
+
+#%% First thing is to make a basic test of the derivative functions
+alpha = -3j - 3
+dalpha = 1e-5
+real_der_num = ((cd_grape_obj.D(alpha+dalpha) - cd_grape_obj.D(alpha))/dalpha)
+real_der_ana = cd_grape_obj.dalphar_dD_mul(alpha)*cd_grape_obj.D(alpha)
+diff = real_der_num - real_der_ana
+diff = diff[:50, :50] #they differ near the edges of the hilbert space
+print(repr(diff))
+print(np.max(np.abs(diff)))
 #%%
-num_test = 4
-alphas = [np.random.uniform(low=-2,high =2) + 1j*np.random.uniform(low=-2,high =2)\
-          for _ in range(num_test)]
-for alpha in alphas:
-    desired_state = qt.tensor(qt.coherent(N,alpha), qt.basis(N2,0))
-    cd_grape_obj.alphas = [alpha,0]
-    e,O = analysis_obj.composite_pulse()
-    psif = sys.simulate_pulse_trotter(e, O, initial_state)
-    fid = qt.fidelity(psif, desired_state)
-    print('fid = %f' % fid)
-    plt.figure()
-    plot_wigner(psif)
-    print(psif.ptrace(1))
-    plt.figure()
-    plot_wigner(desired_state)
-    print(desired_state.ptrace(1))
-    plt.figure()
-    plot_pulse(e,O)
+dalpha = 1e-4*1j
+imag_der_num = ((cd_grape_obj.D(alpha+dalpha) - cd_grape_obj.D(alpha))/np.abs(dalpha))
+imag_der_ana = cd_grape_obj.dalphai_dD_mul(alpha)*cd_grape_obj.D(alpha)
+diff = imag_der_num - imag_der_ana
+diff = diff[:50, :50]  # they differ near the edges of the hilbert space
+print(repr(diff))
+print(np.max(np.abs(diff)))
+#%%
+beta = 4 + 2j
+dbeta = 1e-6
+real_der_num = ((cd_grape_obj.CD(beta + dbeta) - cd_grape_obj.CD(beta))/np.abs(dbeta))
+real_der_ana = cd_grape_obj.dbetar_dCD_mul(beta)*cd_grape_obj.CD(beta)
+diff = real_der_num - real_der_ana
+diff = diff[:10, :10]  # they differ near the edges of the hilbert space
+#print(repr(diff))
+print(np.max(np.abs(diff)))
+#%%
+beta = 2 - 3j
+dbeta = 1e-6*1j
+imag_der_num = ((cd_grape_obj.CD(beta + dbeta) -
+                 cd_grape_obj.CD(beta))/np.abs(dbeta))
+imag_der_ana = cd_grape_obj.dbetai_dCD_mul(beta)*cd_grape_obj.CD(beta)
+diff = imag_der_num - imag_der_ana
+diff = diff[:30, :30]  # they differ near the edges of the hilbert space
+#print(repr(diff))
+print(np.max(np.abs(diff)))
+#%%
+theta = 5
+phi = - -0.365
+dtheta = 1e-3
+der_num = ((cd_grape_obj.R(phi, theta + dtheta) -
+                 cd_grape_obj.R(phi, theta))/np.abs(dtheta))
+der_ana = cd_grape_obj.dtheta_dR_mul(phi, theta)*cd_grape_obj.R(phi, theta)
+diff = der_num - der_ana
+diff = diff[:10, :10]  # they differ near the edges of the hilbert space
+#print(repr(diff))
+print(np.max(np.abs(diff)))
+#%%
+init_params = \
+    np.array(np.concatenate([np.real(cd_grape_obj.alphas), np.imag(cd_grape_obj.alphas),
+                             np.real(cd_grape_obj.betas), np.imag(
+                                 cd_grape_obj.betas),
+                             cd_grape_obj.phis, cd_grape_obj.thetas]), dtype=np.float64)
+
+f, df = cd_grape_obj.cost_function_analytic(init_params)
+
+test_d = np.zeros_like(init_params)
+test_d[4] = 0.001
+f2, df2 = cd_grape_obj.cost_function_analytic(init_params + test_d)
+
+test_grad = (f2-f)/0.001
+print(df)
+print(test_grad)
+print(f)
+print(f2)
+
 # %%