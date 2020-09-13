Change from multipliers to correct derivatives

testing/analytic_opt_testing.py
@@ -9,7 +9,7 @@ import numpy as np
 import qutip as qt
 import matplotlib.pyplot as plt
 #%%
-N = 1  # cavity hilbert space
+N = 60  # cavity hilbert space
 N2 = 2  # qubit hilbert space
 N_blocks = 1
 alpha = 1
@@ -30,7 +30,7 @@ cd_grape_obj = CD_grape(initial_state, target_state, N_blocks,
 alpha = -3j - 3
 dalpha = 1e-5
 real_der_num = ((cd_grape_obj.D(alpha+dalpha) - cd_grape_obj.D(alpha))/dalpha)
-real_der_ana = cd_grape_obj.dalphar_dD_mul(alpha)*cd_grape_obj.D(alpha)
+real_der_ana = cd_grape_obj.dalphar_dD(alpha)
 diff = real_der_num - real_der_ana
 diff = diff[:50, :50] #they differ near the edges of the hilbert space
 print(repr(diff))
@@ -38,7 +38,7 @@ print(np.max(np.abs(diff)))
 #%%
 dalpha = 1e-4*1j
 imag_der_num = ((cd_grape_obj.D(alpha+dalpha) - cd_grape_obj.D(alpha))/np.abs(dalpha))
-imag_der_ana = cd_grape_obj.dalphai_dD_mul(alpha)*cd_grape_obj.D(alpha)
+imag_der_ana = cd_grape_obj.dalphai_dD(alpha)
 diff = imag_der_num - imag_der_ana
 diff = diff[:50, :50]  # they differ near the edges of the hilbert space
 print(repr(diff))
@@ -47,7 +47,7 @@ print(np.max(np.abs(diff)))
 beta = 4 + 2j
 dbeta = 1e-6
 real_der_num = ((cd_grape_obj.CD(beta + dbeta) - cd_grape_obj.CD(beta))/np.abs(dbeta))
-real_der_ana = cd_grape_obj.dbetar_dCD_mul(beta)*cd_grape_obj.CD(beta)
+real_der_ana = cd_grape_obj.dbetar_dCD(beta)
 diff = real_der_num - real_der_ana
 diff = diff[:10, :10]  # they differ near the edges of the hilbert space
 #print(repr(diff))
@@ -57,18 +57,18 @@ beta = 2 - 3j
 dbeta = 1e-6*1j
 imag_der_num = ((cd_grape_obj.CD(beta + dbeta) -
                  cd_grape_obj.CD(beta))/np.abs(dbeta))
-imag_der_ana = cd_grape_obj.dbetai_dCD_mul(beta)*cd_grape_obj.CD(beta)
+imag_der_ana = cd_grape_obj.dbetai_dCD(beta)
 diff = imag_der_num - imag_der_ana
 diff = diff[:30, :30]  # they differ near the edges of the hilbert space
 #print(repr(diff))
 print(np.max(np.abs(diff)))
 #%%
-theta = 5
-phi = - -0.365
+theta = -0.2
+phi =  4.2
 dtheta = 1e-3
 der_num = ((cd_grape_obj.R(phi, theta + dtheta) -
                  cd_grape_obj.R(phi, theta))/np.abs(dtheta))
-der_ana = cd_grape_obj.dtheta_dR_mul(phi, theta)*cd_grape_obj.R(phi, theta)
+der_ana = cd_grape_obj.dtheta_dR(phi, theta)
 diff = der_num - der_ana
 diff = diff[:10, :10]  # they differ near the edges of the hilbert space
 #print(repr(diff))
@@ -79,35 +79,8 @@ phi = -.5
 dphi = 1e-3
 der_num = ((cd_grape_obj.R(phi + dphi, theta) -
             cd_grape_obj.R(phi, theta))/np.abs(dphi))
-der_ana = cd_grape_obj.dphi_dR_mul(phi, theta)
+der_ana = cd_grape_obj.dphi_dR(phi, theta)
 diff = der_num - der_ana
 diff = diff[:10, :10]  # they differ near the edges of the hilbert space
 #print(repr(diff))
 print(np.max(np.abs(diff)))
-#%%
-init_params = \
-    np.array(np.concatenate([np.real(cd_grape_obj.alphas), np.imag(cd_grape_obj.alphas),
-                             np.real(cd_grape_obj.betas), np.imag(
-                                 cd_grape_obj.betas),
-                             cd_grape_obj.phis, cd_grape_obj.thetas]), dtype=np.float64)
-
-f, df = cd_grape_obj.cost_function_analytic(init_params)
-
-test_d = np.zeros_like(init_params)
-test_d[4] = 0.001
-f2, df2 = cd_grape_obj.cost_function_analytic(init_params + test_d)
-
-test_grad = (f2-f)/0.001
-print(df)
-print(test_grad)
-print(f)
-print(f2)
-
-# %%
-sx = qt.sigmax()
-sy = qt.sigmay()
-sz = qt.sigmaz()
-# %%
-def R(phi, theta):
-    return (-1j*(theta/2.0)*(np.cos(phi)*sx + np.sin(phi)*sy)).expm()
-# %%