81ff768db68b24bea2a8911cdf3b8782aa6910c4
awe4 <alec.eickbusch@yale.edu>
2020-10-07 14:13:12 -0400

    examples

--- examples/fock_creation.py
+++ examples/fock_creation.py
@@ -28,7 +28,7 @@ CD_control_obj = CD_control(initial_state, target_state, N_blocks=N_blocks,\
                     saving_directory=saving_directory, use_displacements=False,
                     no_CD_end=True)
 #%% We can plot the initial and target states (qubit traced out)
-if 0:
+if 1:
     plt.figure(figsize=(5,5), dpi=200)
     CD_control_obj.plot_state(i=0, cbar=False)
     plt.title("initial state")
