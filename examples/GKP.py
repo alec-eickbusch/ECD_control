1ae670db6d5dd7fd745f29fbcff719e42e33349d
awe4 <alec.eickbusch@yale.edu>
2020-10-13 09:36:50 -0400

    Working on examples

--- examples/GKP.py
+++ examples/GKP.py
@@ -7,34 +7,45 @@ from CD_control.CD_control_tf import CD_control_tf
 from CD_control.helper_functions import plot_pulse, plot_wigner
 from CD_control.analysis import System, CD_control_analysis
 from CD_control.global_optimization_tf import Global_optimizer_tf
+from bosonic_codes.GKPCode import *
 import numpy as np
 import qutip as qt
 import matplotlib.pyplot as plt
+
 #%%
-N = 50 #cavity hilbert space 
-alpha = 2 + 1j #cat alpha
-N_blocks = 5
+N = 150 #cavity hilbert space 
+c = SquareGKPCode(N=N)
+if 0:
+    c.plot_code_wigner(contour=True)
+#c.plot_mixed_wigner(contour=True)
+#%%
+#O = qt.tensor(qt.identity(2), c.code_projector())
+O = qt.tensor(qt.identity(2), (c.stabilizer_symmetric(i=0) + c.stabilizer_symmetric(i=1) + c.pauli_symmetric(i = 0))/3.0)
+#%%
+N_blocks = 6
 no_CD_end = True
-initial_state = qt.tensor(qt.basis(2,0),qt.basis(N,0))
-target_state = qt.tensor(qt.basis(2,0), (qt.coherent(N,alpha) + qt.coherent(N,-alpha)).unit())
-term_fid = 0.9999
+initial_state = qt.tensor(qt.basis(2,0),qt.squeeze(N,1.5)*qt.basis(N,0))
+target_state = qt.tensor(qt.basis(2,0), c.zero_logical)
+term_fid = 0.999
 #%%
-CD_control_obj = CD_control_tf(initial_state, target_state, N_blocks=N_blocks,
-                    term_fid = term_fid, no_CD_end=True)
+CD_control_obj = CD_control_tf(initial_state = initial_state, target_state = target_state)
+#%%
+CD_control_obj = CD_control_tf(initial_state, target_state,
+                                N_blocks = N_blocks, term_fid=term_fid,
+                                no_CD_end=no_CD_end)
 #%% We can plot the initial and target states (qubit traced out)
 plt.figure(figsize=(5,5), dpi=200)
 CD_control_obj.plot_initial_state()
 plt.title("initial state")
-#plt.figure(figsize=(5, 5), dpi=200)
-#CD_control_obj.plot_target_state()
-#plt.title("target state")
+plt.figure(figsize=(5, 5), dpi=200)
+CD_control_obj.plot_target_state()
+plt.title("target state")
+
 #%%
-CD_control_obj.randomize(beta_scale=1.0)
-fids = CD_control_obj.optimize(epochs = 50, epoch_size=10,dloss_stop=-1)
+CD_control_obj.randomize(beta_scale=0.001)
+fids = CD_control_obj.optimize(epochs = 2000, epoch_size=2, dloss_stop=1e-7, learning_rate=0.001)
 #%%
 CD_control_obj.plot_final_state()
-#%%
-CD_control_obj
 #%%
 CD_control_obj.randomize(beta_scale=1.0)
 betas, phis, thetas = CD_control_obj.get_numpy_vars()
@@ -47,9 +58,9 @@ theta_mask = beta_mask
 #beta_mask[-1] = 0
 CD_control_obj.optimize(beta_mask=beta_mask, theta_mask=theta_mask, learning_rate=1.0)
 #%%
-CD_control_obj.term_fid = 0.999
-fids = CD_control_obj.multistart_optimize(N_multistart = 10, beta_scale=0.5, learning_rate = 0.1,
-df_stop=-1, epoch_size=1, epochs= 300, do_prints=False)
+CD_control_obj.term_fid = 0.99
+fids = CD_control_obj.multistart_optimize(N_multistart = 10, beta_scale=0.5, learning_rate = 0.01,
+df_stop=-1, epoch_size=300, epochs= 100, do_prints=True)
 #%%
 CD_control_obj.term_fid = 0.998
 fids = CD_control_obj.pin_optimize(tolerance=0.1, learning_rate=0.01, df_stop=-1, epoch_size=1, epochs=1000, do_prints=False)
@@ -79,16 +90,16 @@ CD_control_obj.plot_final_state()
 plt.title("final state")
 #%% Now, we can convert these parameters to a pulse we can run on the experiment
 #first, creating a system object
-epsilon_m = 2*np.pi*1e-3*300.0  # maximum displacement rate
-alpha0 = 50  # maximum displacement before things break down
+epsilon_m = 2*np.pi*1e-3*600.0  # maximum displacement rate
+alpha0 = 55  # maximum displacement before things break down
 Ec_GHz = 0.19267571  # measured anharmonicity
 Ec = (2*np.pi) * Ec_GHz
 chi_MHz = 0.03
 chi = 2*np.pi*1e-3*chi_MHz
-sigma = 6  # sigma for gaussian pulses
+sigma = 3  # sigma for gaussian pulses
 chop = 4  # chop for gaussian pulses
-buffer_time = 4  # time between discrete pulses
-ring_up_time = 16  # Time to ring up for large CD pulses
+buffer_time = 0  # time between discrete pulses
+ring_up_time = 4  # Time to ring up for large CD pulses
 sys = System(chi=chi, Ec=Ec, alpha0=alpha0,
              sigma=sigma, chop=chop, epsilon_m=epsilon_m, buffer_time=buffer_time,
              ring_up_time=ring_up_time)
