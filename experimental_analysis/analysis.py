0c2f0d965c7fbd02cc521bdcbffb6a17240052f5
awe4 <alec.eickbusch@yale.edu>
2020-10-29 15:30:57 -0400

    naming

--- experimental_analysis/analysis.py
+++ experimental_analysis/analysis.py
@@ -3,8 +3,8 @@ import numpy as np
 import qutip as qt
 
 # from CD_control.CD_control_optimization import *
-from CD_control.analysis.basic_pulses import fastest_CD, rotate, disp_gaussian
-from CD_control.analysis.helper_functions import alpha_from_epsilon, plot_pulse
+from experimental_analysis.basic_pulses import fastest_CD, rotate, disp_gaussian
+from experimental_analysis.helper_functions import alpha_from_epsilon, plot_pulse
 import matplotlib.pyplot as plt
 
 plt.rcParams.update({"font.size": 16, "pdf.fonttype": 42, "ps.fonttype": 42})
