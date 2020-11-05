ab15290037da8991828224f5b7eaf2c906cd8452
Alec Eickbusch <alec.eickbusch@yale.edu>
2020-11-05 11:18:09 -0500

    alpha from epsilon 2

--- experimental_analysis/helper_functions.py
+++ experimental_analysis/helper_functions.py
@@ -106,15 +106,24 @@ def interp(data_array, dt=1):
 
 
 # still having problems with this version
-"""
-def alpha_from_epsilon(epsilon_array, dt=1, K = 0, alpha_init=0+0j, kappa_cavity=0):
-    t_eval = np.linspace(0,len(epsilon_array)*dt-dt, len(epsilon_array))
+
+
+def alpha_from_epsilon2(
+    epsilon_array, dt=1, delta=0, alpha_init=0 + 0j, kappa_cavity=0
+):
+    t_eval = np.linspace(0, len(epsilon_array) * dt - dt, len(epsilon_array))
     epsilon = interp(epsilon_array, dt)
-    dalpha_dt = lambda t, alpha : -1j*epsilon(t) - (kappa_cavity/2)*alpha# - 1j*4*K*np.abs(alpha)**2 * alpha
-    alpha = solve_ivp(dalpha_dt,(0,len(epsilon_array)*dt-dt),y0=[alpha_init],\
-                      method='RK23',t_eval=t_eval).y[0]
+    dalpha_dt = (
+        lambda t, alpha: -1j * epsilon(t) + delta * alpha - (kappa_cavity / 2) * alpha
+    )  # - 1j*4*K*np.abs(alpha)**2 * alpha
+    alpha = solve_ivp(
+        dalpha_dt,
+        (0, len(epsilon_array) * dt - dt),
+        y0=[alpha_init],
+        method="RK23",
+        t_eval=t_eval,
+    ).y[0]
     return alpha
-"""
 
 
 def alpha_from_epsilon(epsilon):
